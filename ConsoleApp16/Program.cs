23d387c9fdd82257babfd5daab5799d57c6e9eea
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-15 16:10:47 +0300

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialValue = 5;
+            int maximumValue = 96;
+            int step = 7;
+
+            for (int i = initialValue; i <= maximumValue; i += step)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
